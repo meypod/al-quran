57ee8a5ca6bb450cd75e267dd5d8b32d9147a801
Meypod <radical.zero@hotmail.com>
2025-10-05 14:29:05 +0330

    fix: make app use dark and light themes automatically

--- lib/app.dart
+++ lib/app.dart
@@ -15,7 +15,10 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             title: 'Flutter Demo',
             theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.deepPurple,
+                brightness: Brightness.light,
+              ),
               textTheme: TextTheme(
                 displayLarge: TextStyle(fontSize: 32),
                 displayMedium: TextStyle(fontSize: 28),
@@ -34,6 +37,30 @@ class MyApp extends StatelessWidget {
                 labelSmall: TextStyle(fontSize: 10),
               ).apply(fontSizeFactor: state.fontSize / 16.0),
             ),
+            darkTheme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.deepPurple,
+                brightness: Brightness.dark,
+              ),
+              textTheme: TextTheme(
+                displayLarge: TextStyle(fontSize: 32),
+                displayMedium: TextStyle(fontSize: 28),
+                displaySmall: TextStyle(fontSize: 24),
+                headlineLarge: TextStyle(fontSize: 22),
+                headlineMedium: TextStyle(fontSize: 20),
+                headlineSmall: TextStyle(fontSize: 18),
+                titleLarge: TextStyle(fontSize: 18),
+                titleMedium: TextStyle(fontSize: 16),
+                titleSmall: TextStyle(fontSize: 14),
+                bodyLarge: TextStyle(fontSize: 16),
+                bodyMedium: TextStyle(fontSize: 14),
+                bodySmall: TextStyle(fontSize: 12),
+                labelLarge: TextStyle(fontSize: 14),
+                labelMedium: TextStyle(fontSize: 12),
+                labelSmall: TextStyle(fontSize: 10),
+              ).apply(fontSizeFactor: state.fontSize / 16.0),
+            ),
+            themeMode: ThemeMode.system,
             home: const MainPage(),
           );
         },
